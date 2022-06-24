71a790d18f3695dbf1dbb76b38282ef775181d1e
Dmitriy Brilov <dmitrijbrilev6@gmail.com>
2022-06-24 12:54:33 +0200

    Heroku deployment

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const JWTstorage = []
 //sha, message, html url, commiter info, date.
 
 const app = express()
-const PORT = process.env.PORT || 3000
+const port = process.env.PORT || 3000
 
 app.use(cors())
 
@@ -84,5 +84,5 @@ app.use('/graphql', graphqlHTTP({
 }))
 
 
-app.listen(PORT, () => console.log(`Server is up on port ${PORT}`))
+app.listen(port, () => console.log(`Server is up on port ${port}`))
 
